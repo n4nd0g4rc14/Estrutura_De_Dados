29217b5e7c99b9185ad955dc28ce22f37609ef32
n4nd0g4rc14 <128625777+n4nd0g4rc14@users.noreply.github.com>
2023-08-23 20:43:34 -0300

    Add files via upload

--- Aritmetica.java
+++ Aritmetica.java
@@ -17,6 +17,20 @@ class Aritmetica {
         }
         return x * pot(x, (n-1));
     }
+
+   public void inverteArray(double[] v, int esq, int dir);{
+        if (esq >= dir) {
+            return;
+        }
+        double temp = v[esq];
+        v[esq] = v[dir];
+        v[dir] = temp;
+        inverteArray(v, (esq+1), (dir-1));
+   }
+   public void toString(double[] v){
+    for(int i = 0; i < v.length; i++)
+        System.out.print(v[i] + ", ")
+   }
 }
 
 public class Main
@@ -24,11 +38,22 @@ public class Main
 	public static void main(String[] args) {
 		Aritmetica ari = new Aritmetica();
 		Scanner leitor = new Scanner(System.in);
-		int valor = leitor.nextInt();
-		int potencia = leitor.nextInt();
+		//int valor = leitor.nextInt();
+		//int potencia = leitor.nextInt();
 		//int resultado = ari.somaAlgarismos(3338, 0);
 		//int resultado = ari.somaAlgarismos(valor, 0);
-		int resultado = ari.pot(valor, potencia);
-		System.out.println(resultado);
+		//int resultado = ari.pot(valor, potencia);
+		//System.out.println(resultado);
+        double[] vetim = new double[5];
+        vetim[0] = 1.2;
+        vetim[1] = 4.2;
+        vetim[2] = 5.7;
+        vetim[3] = 8.9;
+        vetim[4] = 4.5;
+
+        ari.toString(vetim);
+        ari.inverteArray(vetim, 0, (vetim.length-1));
+        System.out.println();
+        ari.toString(vetim);
 	}
 }
