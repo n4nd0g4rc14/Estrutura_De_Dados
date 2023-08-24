e6490ca5d1f43ca2a93fe112a9bba51229bcc0aa
n4nd0g4rc14 <128625777+n4nd0g4rc14@users.noreply.github.com>
2023-08-23 22:44:14 -0300

    Update Aritmetica.java

--- Aritmetica.java
+++ Aritmetica.java
@@ -1,59 +1,55 @@
-//QUESTÃO 01, 02, 03
-
 import java.util.Scanner;
 
 class Aritmetica {
-    public int somaAlgarismos(int num, int soma){
-        if(num<1){
+    public int somaAlgarismos(int num, int soma) {
+        if (num < 1) {
             return soma;
         }
-        soma+=num%10;
-        return somaAlgarismos((num/10), soma);
-    }
-    
-    public int pot(int x, int n){
-        if(n == 0) {
-            return 1;
-        }
-        return x * pot(x, (n-1));
+        soma += num % 10;
+        return somaAlgarismos(num / 10, soma);
     }
 
-   public void inverteArray(double[] v, int esq, int dir);{
+    public int pot(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * pot(x, n - 1);
+    }
+
+    public void inverteArray(double[] v, int esq, int dir) {
         if (esq >= dir) {
             return;
         }
         double temp = v[esq];
         v[esq] = v[dir];
         v[dir] = temp;
-        inverteArray(v, (esq+1), (dir-1));
-   }
-   public void toString(double[] v){
-    for(int i = 0; i < v.length; i++)
-        System.out.print(v[i] + ", ")
-   }
+        inverteArray(v, esq + 1, dir - 1);
+    }
+
+    public void mostrarArray(double[] v) {
+        for (double valor : v) {
+            System.out.print(valor + ", ");
+        }
+    }
 }
 
-public class Main
-{
-	public static void main(String[] args) {
-		Aritmetica ari = new Aritmetica();
-		Scanner leitor = new Scanner(System.in);
-		//int valor = leitor.nextInt();
-		//int potencia = leitor.nextInt();
-		//int resultado = ari.somaAlgarismos(3338, 0);
-		//int resultado = ari.somaAlgarismos(valor, 0);
-		//int resultado = ari.pot(valor, potencia);
-		//System.out.println(resultado);
-        double[] vetim = new double[5];
-        vetim[0] = 1.2;
-        vetim[1] = 4.2;
-        vetim[2] = 5.7;
-        vetim[3] = 8.9;
-        vetim[4] = 4.5;
+public class Main {
+    public static void main(String[] args) {
+        Aritmetica ari = new Aritmetica();
+        Scanner leitor = new Scanner(System.in);
 
-        ari.toString(vetim);
-        ari.inverteArray(vetim, 0, (vetim.length-1));
+        // int valor = leitor.nextInt();
+        // int potencia = leitor.nextInt();
+        // int resultado = ari.somaAlgarismos(3338, 0);
+        // int resultado = ari.somaAlgarismos(valor, 0);
+        // int resultado = ari.pot(valor, potencia);
+        // System.out.println(resultado);
+
+        double[] vetim = {1.2, 4.2, 5.7, 8.9, 4.5};
+
+        ari.mostrarArray(vetim);
+        ari.inverteArray(vetim, 0, vetim.length - 1);
         System.out.println();
-        ari.toString(vetim);
-	}
+        ari.mostrarArray(vetim);
+    }
 }
